3dd81f24588e22bea3e4a2346db7373f4561cff1
sonaisrayel <sonai@brightcom.com>
2023-10-10 16:51:38 +0400

    Add ananimous function

--- async.js
+++ async.js
@@ -1,21 +1,28 @@
 function fetchDataFromAPI(url) {
-    return fetch(url)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-      });
-  }
-  function fetchMultipleAPIs(apiUrls) {
-    const promises = apiUrls.map(url => fetchDataFromAPI(url));
-    return Promise.all(promises);
-  }
-  const apiUrls = [
-    'https://jsonplaceholder.typicode.com/posts/4',
-    'https://jsonplaceholder.typicode.com/posts/5',
-    'https://jsonplaceholder.typicode.com/posts/6'
-  ];
+  return fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    });
+}
+
+
+
+function fetchMultipleAPIs(apiUrls) {
+  const promises = apiUrls.map(url => fetchDataFromAPI(url));
+  return Promise.all(promises);
+}
+
+
+const apiUrls = [
+  'https://jsonplaceholder.typicode.com/posts/4',
+  'https://jsonplaceholder.typicode.com/posts/5',
+  'https://jsonplaceholder.typicode.com/posts/6'
+];
+
+
 //   fetchMultipleAPIs(apiUrls)
 //     .then(results => {
 //       console.log('Combined Results:', results);
@@ -24,13 +31,29 @@ function fetchDataFromAPI(url) {
 //       console.log('Error:', error.message);
 //     });
 
-async () => {
-    try {
-        const value  = await fetchMultipleAPIs(apiUrls);
-        return ('Combined Results:', results)
-    }
- catch(err){
-    console.log('Error:', error.message)
+
+async function bubu() {
+  try {
+    const value = await fetchMultipleAPIs(apiUrls);
+    console.log('Combined Results:', value)
+  }
+  catch (err) {
+    console.log('Error:', err.message)
+  }
 }
-}
- fetchMultipleAPIs(apiUrls)
+
+bubu()
+
+
+// (async () => {
+//   try {
+//     const value = await fetchMultipleAPIs(apiUrls);
+//     console.log ('Combined Results:', value)
+//   }
+//   catch (err) {
+//     console.log('Error:', err.message)
+//   }
+// })()
+
+
+//fetchMultipleAPIs(apiUrls)
